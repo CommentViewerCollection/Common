dad36b7183266a5b188fb14cffb0e3e8d6afb7ff
Ryu Shinmura <rysestock@gmail.com>
2018-11-17 21:22:23 +0900

    LoggerTestのテストを追加

---
+++ CommonTests/LoggerTestTests.cs
@@ -0,0 +1,42 @@
+﻿using Common;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonTests
+{
+    [TestFixture]
+    class LoggerTestTests
+    {
+        [Serializable]
+        class ParseException : Exception
+        {
+            public string Raw { get; }
+            public ParseException(string raw)
+            {
+                Raw = raw;
+            }
+            public ParseException(string raw, Exception inner) : base("", inner)
+            {
+                Raw = raw;
+            }
+        }
+        [Test]
+        public void 例外のプロパティの値が記録されているか()
+        {
+            var logger = new LoggerTest();
+            try
+            {
+                throw new ParseException("abc");
+            }
+            catch (ParseException ex)
+            {
+                logger.LogException(ex);
+            }
+            Assert.IsTrue(logger.GetExceptions().Contains("\"Raw\":\"abc\""));
+        }
+    }
+}
